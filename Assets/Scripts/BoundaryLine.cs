673256440113a6e7766985567fb01a7d7226660a
Laurenceb5033698 <b5033698@my.shu.ac.uk>
2017-01-10 14:06:57 +0000

    Fixed Level Boundary and Fixed Merge
    some desync sstuff going on with git. should be fixed now.

--- Assets/Scripts/BoundaryLine.cs
+++ Assets/Scripts/BoundaryLine.cs
@@ -5,42 +5,48 @@ public class BoundaryLine : MonoBehaviour {
 
     public GameObject ship;
     private LineRenderer line;
-    //private LineRenderer linex;
 
     public bool drawLine = false;
     public bool ZORX;
 
     private const int SBOUND = 600;
     private const int HBOUND = SBOUND + 40;
-    private Vector3 lineStartPos = new Vector3(0,40,0);
-    private Vector3 lineEndPos = new Vector3(0,40,0);
-	// Use this for initialization
+    private Vector3 lineStartPos = new Vector3(0, 40, 0);
+    private Vector3 lineEndPos = new Vector3(0, 40, 0);
+
+    // Use this for initialization
     void Start()
     {
         line = transform.gameObject.GetComponent<LineRenderer>();
         line.SetWidth(0.2f, 0.2f);
-        //linez = transform.gameObject.AddComponent<LineRenderer>();
-        //linex.SetWidth(0.2f, 0.2f);
+
         drawLine = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if (drawLine){ //draws a line on the z border
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (drawLine)
+        { //draws a line on the z border
             if (ZORX)
                 drawboundaryZ();
             else
                 drawboundaryX();
         }
-	}
+    }
     private void drawboundaryZ()
     {
         int zside = 1;
         if (ship.transform.position.z < 0)
             zside = -1;
-        lineStartPos = new Vector3(ship.transform.position.x - 40, 0, zside * SBOUND);
-        line.SetPosition(0, lineStartPos);
+        lineStartPos = new Vector3(ship.transform.position.x - 40, 0, zside * HBOUND);
         lineEndPos = lineStartPos + new Vector3(80, 0, 0);
+        if (lineStartPos.x < -HBOUND)
+            lineStartPos.x = -HBOUND;
+        else if (lineEndPos.x > HBOUND)
+            lineEndPos.x = HBOUND;
+
+        line.SetPosition(0, lineStartPos);
         line.SetPosition(1, lineEndPos);
     }
     private void drawboundaryX()
@@ -48,19 +54,19 @@ public class BoundaryLine : MonoBehaviour {
         int xside = 1;
         if (ship.transform.position.x < 0)
             xside = -1;
-        lineStartPos = new Vector3(xside * SBOUND, 0, ship.transform.position.z - 40);
+        lineStartPos = new Vector3(xside * HBOUND, 0, ship.transform.position.z - 40);
         lineEndPos = lineStartPos + new Vector3(0, 0, 80);
-        if (Mathf.Abs(lineStartPos.z) < SBOUND)
-            lineStartPos.z = -SBOUND;
-        else if (Mathf.Abs(lineEndPos.z) > SBOUND)
-            lineEndPos.z = SBOUND;
+        if (lineStartPos.z < -HBOUND)
+            lineStartPos.z = -HBOUND;
+        else if (lineEndPos.z > HBOUND)
+            lineEndPos.z = HBOUND;
         line.SetPosition(0, lineStartPos);
         line.SetPosition(1, lineEndPos);
     }
 
     public bool drawstate
     {
-        get { return drawLine;  }
+        get { return drawLine; }
         set { drawLine = value; }
     }
 
