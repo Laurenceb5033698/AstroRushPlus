843c4eb37d74d4fbce0e45725ad22539e1228e26
GergelyDaku <gery1277@gmail.com>
2017-01-12 20:10:26 +0000

    Up To Date Version Merge
    inProgress PathFinding - infinite loop is still active!

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; 
+
 
 public class EnemyAI : MonoBehaviour {
 
@@ -18,6 +20,13 @@ public class EnemyAI : MonoBehaviour {
     private float timer;
     // 0 = idle, 1 = searchForTarget, 2 = patrolArond, 3 = gotoDestination, 4 = attackTarget, 5 = returnHome
 
+    //pathing variables
+
+    private RaycastHit pathObjectHitInfo;
+    private Vector3 newDest;
+    [SerializeField] private List<Vector3> pathList = new List<Vector3>();
+    //private List<Vector3> PatrolPathList = new List<Vector3>();
+    
 
 	// Use this for initialization
 	void Start ()
@@ -26,9 +35,12 @@ public class EnemyAI : MonoBehaviour {
         rb = ship.gameObject.GetComponent<Rigidbody>();
         destination = ship.transform.position;
         origin = ship.transform.position;
-        target = null;
+        //target = null;
         timer = Time.time;
         state = 0;
+        
+        
+
 
         laser = transform.gameObject.GetComponent<LineRenderer>();
         laser.SetWidth(0.2f, 0.2f);
@@ -37,10 +49,14 @@ public class EnemyAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        laserIsOn = (state == 4) ? true : false;
-        DrawLaser();
-
-        UpdateState();
+        pathList.Clear();
+        checkDest();
+        //laserIsOn = (state == 4) ? true : false;
+        //DrawLaser();
+        
+        //Debug.Log(rayCheck(ship.transform.position, target.transform.position));
+        drawPath();
+        //UpdateState();
     }
 
     private void UpdateState()
@@ -127,16 +143,109 @@ public class EnemyAI : MonoBehaviour {
     private void AttackTarget()
     {
         DrawLaser();
-        ship.transform.position = Vector3.MoveTowards(ship.transform.position, target.transform.position, 5f * Time.deltaTime);
-        ship.transform.LookAt(target.transform.position);
+        //ship.transform.position = Vector3.MoveTowards(ship.transform.position, target.transform.position, 5f * Time.deltaTime);
+        //ship.transform.LookAt(target.transform.position);
+        ship.transform.rotation = Quaternion.RotateTowards(ship.transform.rotation, Quaternion.LookRotation(target.transform.position - ship.transform.position), Time.deltaTime * 32f);
     }
 
 
+    private bool rayCheck(Vector3 startPos, Vector3 destPos)
+    {
+        Ray pathObject = new Ray(startPos, startPos + (destPos - startPos));
+        
+        bool hit = Physics.Raycast(pathObject, out pathObjectHitInfo);
+        if (hit)
+            return (!(pathObjectHitInfo.transform.gameObject == target) && (Vector3.Distance(startPos, destPos) > Vector3.Distance(startPos, pathObjectHitInfo.transform.gameObject.transform.position)));
+        else
+            return false;
+        // || (Vector3.Distance(startPos, destPos) > Vector3.Distance(startPos, pathObjectHitInfo.transform.gameObject.transform.position))
+    }
+    private void checkDest(){
+        //only run this function once to get path.
+        if (rayCheck(ship.transform.position,target.transform.position))
+        {//an obstacle was detected
 
+            //generates new path points to avoid obstacle
+            //checks if path to the newdest is ok
+            checkPath(ship.transform.position);
+            //if one is ok then adds newdest to pathlist
+        }
 
+        if(pathList.Count > 0){//if there are any path points in the list
+            while (rayCheck(pathList[pathList.Count - 1], target.transform.position))//for every
+            {
+                checkPath(pathList[pathList.Count - 1]);
+                Debug.Log("inside while");
+            }
+        }
 
+        //int temp = pathList.FindLast();
+        pathList.Add(target.transform.position);
+        //pathList.Reverse();
 
-    // ---VVVVVVVVV----- This part is copied from laser class for testing only
+        //add path points to patrolpath in reverse order
+        //PatrolPathList.Add(target.transform.position);
+        //for (int i = pathList.Count; i > 0; i--)
+        //{
+        //    PatrolPathList.Add(pathList[i]);
+        //    pathList.RemoveAt(i);
+        //}
+        
+    }
+
+    private Vector3 generateNewPoint(Vector3 start)
+    {
+        //take pathObjectHitInfo.transform.gameObject;
+        //maths to add distance perpendicular to raydirection
+        float radius = Vector3.Distance(pathObjectHitInfo.point, pathObjectHitInfo.transform.gameObject.transform.position);
+        Vector3 perp = Vector3.Cross((target.transform.position - start), Vector3.up).normalized;
+        return (pathObjectHitInfo.transform.gameObject.transform.position +( perp * radius *2f));
+        //should return a point that is 15 units 
+    }
+
+    private void checkPath(Vector3 start)
+    {
+        int checkPathIndex = 0;//every fail adds to this
+        int failSafe = 10;
+        int counter = 0;
+
+        do
+        {
+            newDest = generateNewPoint(start);
+            checkPathIndex++;
+            counter++;
+        } while (rayCheck(start, newDest));
+        //} while (rayCheck(start, newDest) && (pathObjectHitInfo.distance < Vector3.Distance(start, newDest)));
+        //} while (((rayCheck(start, newDest))&&(checkPathIndex<4)) && counter < failSafe);
+        //
+        if (checkPathIndex > 4)
+        {
+            ;//state=destroyOriginalAsteroid;
+            //re-raycasts and destroys initial object that was in it's way
+        }
+        else
+        {
+            pathList.Add(newDest);
+            ;//list.add(newDest);
+        }
+    }
+
+    private void drawPath()
+    {
+        
+            laser.SetVertexCount(pathList.Count +1);
+            var points = new Vector3[pathList.Count + 1];
+
+            points[0] = ship.transform.position;
+            points[1] = ship.transform.position;
+            for (int i = 0; i < pathList.Count; i++)
+            {
+                points[i+1] = pathList[i];
+            }
+
+            laser.SetPositions(points);
+    }     
+    
 
     [SerializeField]
     private GameObject laserGo;
@@ -152,16 +261,17 @@ public class EnemyAI : MonoBehaviour {
     RaycastHit hitInfo;
     Ray detectObject;
     bool hit = false;
+    float laserRange = 25f;
 
     private void DrawLaser()
     {
         if (laserIsOn) // if the laser is on
         {
             // find target -------------------
-            detectObject = new Ray(laserGo.transform.position, laserGo.transform.right * 50f);
+            detectObject = new Ray(laserGo.transform.position, laserGo.transform.right * laserRange);
             hit = Physics.Raycast(detectObject, out hitInfo);
 
-            if (hit && Vector3.Distance(laserGo.transform.position, hitInfo.point) < 50f)
+            if (hit && Vector3.Distance(laserGo.transform.position, hitInfo.point) < laserRange)
                 laserTarget = hitInfo.transform.gameObject;
             else
                 laserTarget = null;
@@ -171,7 +281,7 @@ public class EnemyAI : MonoBehaviour {
 
             if (laserTarget != null)                                                                                                 // if there is something infront
             {
-                if (Vector3.Distance(laserGo.transform.position, hitInfo.point) < 50f)                           // and in range
+                if (Vector3.Distance(laserGo.transform.position, hitInfo.point) < laserRange)                           // and in range
                 {
                     laser.SetPosition(1, hitInfo.point);                                                                        // set the laser to point to that area
                     laser.GetComponent<Renderer>().material = (laserTarget == player) ? activeLaserColor : idleLaserColor;  // set the color of the laser
@@ -179,7 +289,7 @@ public class EnemyAI : MonoBehaviour {
             }
             else                                                                                                                // if there is nothing in front
             {
-                laser.SetPosition(1, (laserGo.transform.position + laserGo.transform.right * 50f));                   // set the laser to max range
+                laser.SetPosition(1, (laserGo.transform.position + laserGo.transform.right * laserRange));                   // set the laser to max range
                 laser.GetComponent<Renderer>().material = idleLaserColor;                                                       // and change color to idle
             }
 
