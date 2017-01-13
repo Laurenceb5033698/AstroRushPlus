e472679ead500bad85441cd887770e523329bea3
Laurenceb5033698 <b5033698@my.shu.ac.uk>
2017-01-13 15:25:37 +0000

    Post merge fixes

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -3,32 +3,43 @@ using System.Collections;
 using UnityEngine.UI;
 using System;
 
-public class ShipController : MonoBehaviour 
+public class ShipController : MonoBehaviour
 {
-	[SerializeField] private GameObject ship;  // ship gameobject
-    [SerializeField] private GameObject station;
-    [SerializeField] private GameObject mPreF; // missile prefab
-	[SerializeField] private Inputs controls;
-	[SerializeField] private AnimateThrusters thrusters;
-	[SerializeField] private Rigidbody rb; 	// ship's rigid body
-	[SerializeField] private Laser shipLaser;
-    [SerializeField] private ShipStats stats;
-    [SerializeField] private UI ui;
-    [SerializeField] private Camera cam;
-    [SerializeField] private GameObject boundaryz;
-    [SerializeField] private GameObject boundaryx;
-    [SerializeField] private GameObject shieldSphere = null;
+    [SerializeField]
+    private GameObject ship;  // ship gameobject
+    [SerializeField]
+    private GameObject mPreF; // missile prefab
+    [SerializeField]
+    private Inputs controls;
+    [SerializeField]
+    private AnimateThrusters thrusters;
+    [SerializeField]
+    private Rigidbody rb; 	// ship's rigid body
+    [SerializeField]
+    private Laser shipLaser;
+    [SerializeField]
+    private ShipStats stats;
+    [SerializeField]
+    private UI ui;
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private GameObject boundaryz;
+    [SerializeField]
+    private GameObject boundaryx;
+    [SerializeField]
+    private GameObject shieldSphere = null;
 
     private const int SBOUND = 600;
     private const int HBOUND = SBOUND + 70;
     private float rotFix = 0f;
 
     // Mains --------------------------------------------------------------------------------------------------------
-    void Start () // Use this for initialization
+    void Start() // Use this for initialization
     {
 
-	}
-	void Update () // Update is called once per frame
+    }
+    void Update() // Update is called once per frame
     {
         controls.UpdateInputs();
         thrusters.UpdateThrusters();
@@ -43,22 +54,22 @@ public class ShipController : MonoBehaviour
             MoveShip();
         }
         else
-        { 
+        {
             thrusters.SetThrusterState(false);
             ui.setMessage(0);
             ui.menu = true;
         }
 
-        UpdateUI ();
+        UpdateUI();
         UpdateBoundary();
 
     }
 
     void FixedUpdate()
     {
-        
+
     }
-	
+
     // FUNCTIONS --------------------------------------------------------------------------------------------------------	
     public void ShieldSphereOpacity()
     {
@@ -83,31 +94,30 @@ public class ShipController : MonoBehaviour
         }
     }
 
-	private void CheckInputs()
-	{
-		if (controls.reset) ResetShip();
+    private void CheckInputs()
+    {
+        if (controls.reset) ResetShip();
 
-        ToggleStationPanel();
         stats.LaserState = controls.RLaser;
-			
-		if (controls.rocket) 
-		{
-			if (stats.LoadMissile ()) 
-			{
-				SpawnMissile ();
-				stats.DecreaseMissileAmount ();
-			} 
-			else
+
+        if (controls.rocket)
+        {
+            if (stats.LoadMissile())
+            {
+                SpawnMissile();
+                stats.DecreaseMissileAmount();
+            }
+            else
             {
                 Debug.Log("Out of Missiles!");
             }
-		}
-	}
-	private void MoveShip()
+        }
+    }
+    private void MoveShip()
     {
         CorrectShipTransforms();
 
-       
+
 
         if (controls.boost && !stats.bco && stats.GetBoostFuelAmount() > 0.0f)
         {
@@ -167,8 +177,8 @@ public class ShipController : MonoBehaviour
         Vector3 locVel = transform.InverseTransformDirection(rb.velocity);
         //Debug.Log(locVel.z);
 
-        locVel.z += -(locVel.z/100*80)*Time.deltaTime;
-        rb.velocity = transform.TransformDirection(locVel);     
+        locVel.z += -(locVel.z / 100 * 80) * Time.deltaTime;
+        rb.velocity = transform.TransformDirection(locVel);
     }
 
 
@@ -196,25 +206,24 @@ public class ShipController : MonoBehaviour
         stats.ResetShip();
     }
 
-    private void SpawnMissile ()
-	{
-		Vector3 shipPos = ship.transform.position;
-		Vector3 shipDirection = ship.transform.right;
-		Quaternion shipRotation = ship.transform.rotation;
-		Vector3 RocketRot = shipPos + (-shipDirection);
+    private void SpawnMissile()
+    {
+        Vector3 shipPos = ship.transform.position;
+        Vector3 shipDirection = ship.transform.right;
+        Quaternion shipRotation = ship.transform.rotation;
+        Vector3 RocketRot = shipPos + (-shipDirection);
 
 
-		GameObject temp = (GameObject)Instantiate (mPreF,shipPos + (shipDirection * 6f),shipRotation);
+        GameObject temp = (GameObject)Instantiate(mPreF, shipPos + (shipDirection * 6f), shipRotation);
         temp.GetComponent<Rigidbody>().AddForce(ship.transform.position + ship.transform.right * (rb.velocity.magnitude * 5f));
-		temp.transform.LookAt(RocketRot);
-		temp.transform.Rotate (-90,0,0);
-	}
+        temp.transform.LookAt(RocketRot);
+        temp.transform.Rotate(-90, 0, 0);
+    }
 
 
     private void UpdateUI()
     {
-        string tempCargo = "" + stats.ShipCargo.ToString("F2") + "/" + stats.GetMaxCargoSpace();
-        ui.UpdateShipStats(stats.Units, stats.ShipFuel, tempCargo, stats.ShipHealth);
+        ui.UpdateShipStats(stats.ShipFuel, stats.ShipHealth);
     }
     private void UpdateBoundary()
     {
@@ -236,89 +245,20 @@ public class ShipController : MonoBehaviour
             stats.takeDamage(10.0f * Time.deltaTime); //every sec ship takes 5% damage
         }
     }
-    private void ToggleStationPanel()
-    {
-        if (Vector3.Distance(ship.transform.position, station.transform.position) < 20f)
-        {
-            ui.UpdateStationPanelToggle(true);
 
-            if (Input.GetAxis("DPadYAxis") > 0)
-            {
-                BuyButton();
-            }
-            if (Input.GetAxis("DPadYAxis") < 0)
-            {
-                SellButton();
-            }
-            if (Input.GetAxis("DPadXAxis") > 0)
-            {
-                RepairButton();
-            }
 
-            Quaternion temp = Quaternion.LookRotation(-station.transform.forward);
-            ship.transform.rotation = Quaternion.RotateTowards(ship.transform.rotation, temp, 10f * Time.deltaTime);
-            ship.transform.position = Vector3.MoveTowards(ship.transform.position,station.transform.position,0.5f*Time.deltaTime);
-
-        }
-        else
-        {
-            ui.UpdateStationPanelToggle(false);
-        }
-    }
-    
-    
     // EVENT HANDLERS-------------------------------------------------------------------------------------
     void OnCollisionEnter(Collision c)
     {
         stats.takeDamage(c.relativeVelocity.magnitude / 2);
         ShieldSphereOpacity();
     }
+
     public void TakeDamage(float amount)
     {
         stats.takeDamage(amount);
         ShieldSphereOpacity();
 
     }
-	void BuyButton()
-	{
-
-		Debug.Log ("buy button is pressed");
-		int tempPrice = station.GetComponent<StationManager> ().GetFuelPrice ();
-		int tempCost = Convert.ToInt32 ((100 - stats.ShipFuel) * tempPrice);
-        
-
-		if (tempCost <= stats.Units) 
-		{
-            stats.Units = -tempCost; // in the background (units += -tempcost)
-			stats.ShipFuel = (100 - stats.ShipFuel);
-		} 
-		else 
-		{
-			stats.ShipFuel = (stats.Units/tempPrice);
-		}
-	}
-	void SellButton()
-	{
-        Debug.Log("sell button is pressed");
-        int tempUnits = Convert.ToInt32(stats.ShipCargo * station.GetComponent<StationManager>().GetCargoPrice());
-		stats.Units = tempUnits;
-        stats.ShipCargo = -stats.ShipCargo;
-	}
-	void RepairButton()
-	{
-        Debug.Log("repair button is pressed");
-        int tempPrice = station.GetComponent<StationManager> ().GetCargoPrice ();
-		int tempCost = (int)((100 - stats.ShipHealth) * tempPrice);
-
-		if (tempCost <= stats.Units) 
-		{
-            stats.Units = -tempCost;
-            stats.ShipHealth = 100f; // set ship damage value to -ship damage value. by doing this the value will be 0
-		} 
-		else 
-		{
-            stats.ShipHealth = (stats.Units / tempPrice);
-		}
-	}
 
 }
