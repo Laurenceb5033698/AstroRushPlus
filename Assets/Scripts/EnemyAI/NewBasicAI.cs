1c1f25cfab1ff00a2c4956e9f7545a0ddc867d66
Laurenceb5033698 <b5033698@my.shu.ac.uk>
2017-01-17 13:52:21 +0000

    Another AI
    Try to start an AI that rams play, and avoids colliding with asteroids.

---
+++ Assets/Scripts/EnemyAI/NewBasicAI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBasicAI : MonoBehaviour {
+
+    [SerializeField] private GameObject ship;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 destination;
+    [SerializeField] private int state = 0;
+    [SerializeField] private GameObject sceneManager;
+
+
+    private EnemyStats stats;
+
+    [SerializeField]
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
