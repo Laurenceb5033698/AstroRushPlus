22553932fee357f949d091963a3ca3e93b052dfa
TheEryn <cdanielhiggins@gmail.com>
2025-06-28 19:52:34 +0100

    UI - IDK
    Attempted to add a warp progress tracker, and fading text script, neither are very good

---
+++ Assets/Assets_Artist/Scripts/Script_UI_TMP_FadeInAndOut.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System;
+using System.Threading;
+public class Script_UI_TMP_FadeInAndOut : MonoBehaviour
+{
+
+    public TextMeshProUGUI textDisplay;
+    public float FadeInDuation;
+    public float FadeOutDuation;
+       public float TextLengthDuration;
+    public void Start()
+    {
+        StartCoroutine(FadeIn());
+        //StartCoroutine(FadeIn());
+        textDisplay.alpha = 0f;
+    }
+
+    //private IEnumerator FadeIn()
+    //{
+    //textDisplay.CrossFadeAlpha(0, 2f, true);
+    //}
+
+    private void Update()
+    {
+        TextLengthDuration -= Time.deltaTime;
+        if (TextLengthDuration <= 0.0f)
+        {
+            TimerEnded();
+        }
+    }
+
+   private IEnumerator FadeIn()
+    {
+        float currentTime = 0f;
+        {
+         float alpha = Mathf.Lerp(0f, 1f, currentTime / FadeInDuation);
+            textDisplay.color = new Color(textDisplay.color.r, textDisplay.color.g, textDisplay.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+    private void TimerEnded()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+
+    private IEnumerator FadeOut()
+    {
+        float currentTime = 0f;
+
+        while (currentTime < FadeOutDuation)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / FadeOutDuation);
+            textDisplay.color = new Color(textDisplay.color.r, textDisplay.color.g, textDisplay.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+}
