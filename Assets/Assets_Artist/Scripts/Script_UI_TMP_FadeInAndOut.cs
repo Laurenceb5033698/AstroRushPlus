acab71e45c01e5600cf65ac8df5a6faf65726c44
TheEryn <cdanielhiggins@gmail.com>
2025-06-29 12:29:56 +0100

    UI - Small Update
    Small UI Update

--- Assets/Assets_Artist/Scripts/Script_UI_TMP_FadeInAndOut.cs
+++ Assets/Assets_Artist/Scripts/Script_UI_TMP_FadeInAndOut.cs
@@ -3,31 +3,46 @@ using TMPro;
 using System.Collections;
 using System;
 using System.Threading;
+using UnityEngine.Analytics;
 public class Script_UI_TMP_FadeInAndOut : MonoBehaviour
 {
 
     public TextMeshProUGUI textDisplay;
+    public GameObject TriggerNextText;
     public float FadeInDuation;
     public float FadeOutDuation;
     public float TextLengthDuration;
+    public Boolean TriggerSelf;
+    private Boolean TextLive = false;
+
+    
     public void Start()
     {
-        StartCoroutine(FadeIn());
-        //StartCoroutine(FadeTextIn());
         textDisplay.alpha = 0f;
+
+        if (TriggerSelf == true)
+        {
+            StartTextAnimation();
+        }
     }
 
-    //private IEnumerator FadeTextIn()
-    //{
-    //textDisplay.CrossFadeAlpha(0, 2f, true);
-    //}
+    public void StartTextAnimation()
+    {
+            StartCoroutine(FadeIn());
+            textDisplay.alpha = 0f;
+            TextLive = true;
+    }
+
 
     private void Update()
     {
-        TextLengthDuration -= Time.deltaTime;
-        if (TextLengthDuration <= 0.0f)
+        if (TextLive == true)
         {
-            TimerEnded();
+            TextLengthDuration -= Time.deltaTime;
+            if (TextLengthDuration <= 0.0f)
+            {
+                TimerEnded();
+            }
         }
     }
 
@@ -63,4 +78,9 @@ public class Script_UI_TMP_FadeInAndOut : MonoBehaviour
         }
         yield break;
     }
+
+   private void TriggerNext()
+  {
+    
+  }
 }
